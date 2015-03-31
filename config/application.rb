36b5db1b37940ea79f174713bf0d04e0407bf655
Michael Foster <michael.foster@gmail.com>
2015-03-31 10:00:27 -0700

    extend assets to include vendor

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(:default, Rails.env)
 module Yelfie
   class Application < Rails::Application
 
-    config.assets.paths << Rails.root.join("app", "vendor", "assets", "images")
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
