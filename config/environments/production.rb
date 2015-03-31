9935b6dcb52bd228bfeede379968208488c41592
Michael Foster <michael.foster@gmail.com>
2015-03-30 18:14:34 -0700

    prod env changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,10 @@ Yelfie::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
+
+  config.static_cache_control = "public, max-age=2592000"
+  config.action_controller.perform_caching = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -33,7 +36,7 @@ Yelfie::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  # config.assets.version = '1.0'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -64,6 +67,17 @@ Yelfie::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+      address: "in-v3.mailjet.com",
+      port: 587,
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['SMTP_USER'],
+      password: ENV['SMTP_PASS']
+  }
+
+  config.action_mailer.default_url_options = { host: 'mikefoster.co', port: 80 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
